9cd0f042b90895b79084c258b0cc6b3f3b2207f7
William Hebaptec <wjhebaptec@users.noreply.github.com>
2026-05-15 06:10:23 +0800

    chore: add reminder to check Octopus Agile tariff API compatibility

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 // TODO: look into contributing SolarEdge SE7600H inverter fixes upstream.
 // TODO: investigate min-soc behavior when grid price spikes overnight.
 // TODO: test new push notification support for charge-complete alerts.
+// TODO: check if the new tariff API changes break my Octopus Agile setup.
 func main() {
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
